ab46d7cdc4ad08f68be039d869a093b77bdaf84f
sivarajsiva <sivaraj322@gmail.com>
2022-05-18 11:25:57 +0530

    Update AppsTest.java

--- src/test/java/com/mycompany/app/AppsTest.java
+++ src/test/java/com/mycompany/app/AppsTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+public class AppsTest
 {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
